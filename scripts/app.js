6330141a5a1c8ae5a2bbba74a752d34131f171cd
Voanhnguyen <voanhnguyenxd@gmail.com>
2021-11-17 14:50:18 +0700

    landing page

--- scripts/app.js
+++ scripts/app.js
@@ -66,3 +66,14 @@
 // //   //   console.log("on");
 // //   // });
 // // }
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+const activeClass = "is-show";
+toggle.addEventListener("click", function () {
+  menu.classList.add(activeClass);
+});
+window.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+    menu.classList.remove(activeClass);
+  }
+});
